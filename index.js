62d0eaa0058994e0e3e3ca4e136fe70ffd5fa09c
jjfitz <jason.j.fitzgerald@gmail.com>
2017-07-05 23:34:11 -0600

    Milestone 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var {Pool} = require('pg') // that gets the Pool attribute from the full pg module
 
@@ -16,9 +18,22 @@ if (process.env.DATABASE_URL != '')
   var connectionString = process.env.DATABASE_URL;
 } else {
 var connectionString = 'postgres://postgres@localhost/gonning';
-}
+//}
 
 const pool = new Pool({connectionString});
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -33,18 +48,47 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/user', function(request, response){
-  const id = request.query.id;
-  pool.query('SELECT * FROM "users" WHERE id = $1', [id], (err, res) => {
+app.post('/checkUser', function(request, response){
+  handleRequest(request, response);
+});
+
+function handleRequest(request, response) {
+console.log(request.body.username);
+  const username = request.body.username;
+  pool.query('SELECT * FROM "users" WHERE user_name = $1', [username], (err, res) => {
     if (err) {
       throw err
     } else {
       console.log(res);
-      response.json(res.rows[0]);
+      //response.json(res.rows[0]);
+      var user = res.rows[0];
+      console.log(user);
+      showGoals(response, user);
     }
   })
-  
-});
+}
+
+function showGoals(response, user) {
+  pool.query('SELECT * FROM "goals" WHERE user_id = $1', [user.id], (err, res) => {
+    if (err) {
+      throw err
+    } else {
+      console.log(res);
+      var goals = [];
+      //response.json(res.rows[0]);
+      for (i = 0; i < res.rows.length; i++) {
+        console.log(res.rows[i]);
+        goals.push(res.rows[i]);
+        console.log(goals[i]);
+      } 
+      //var goals[0] = res.rows[0];
+      var params = {user: user, goals: goals};
+      response.render('pages/goals', params);
+    }
+  })
+  //var params = {user: user, goals: goals};
+  //response.render('pages/goals', params);
+}
 
 app.get('/goals', function(request, response){
   const id = request.query.id;
