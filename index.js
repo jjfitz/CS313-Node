544fabfda728d3fcd2211778b8e400265b4cd7f7
jjfitz <jason.j.fitzgerald@gmail.com>
2017-07-19 07:23:11 -0600

    Goal view works and inserting goals

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ var {Pool} = require('pg') // that gets the Pool attribute from the full pg modu
 
 //Pool.defaults.ssl = true;
 
-
 var gmaps = require('@google/maps').createClient({
   key: 'my key'
 });
 
+app.use(session({
+  secret: 'Achiever',
+  resave: false,
+  saveUninitialized: true
+}))
+
 if (process.env.DATABASE_URL != '')
 {
   var connectionString = process.env.DATABASE_URL;
-} else {
-var connectionString = 'postgres://postgres@localhost/gonning';
-//}
+} 
+// else {
+// var connectionString = 'postgres://postgres@localhost/gonning';
+// }
 
 const pool = new Pool({connectionString});
 
@@ -49,23 +55,52 @@ app.get('/', function(request, response) {
 });
 
 app.post('/checkUser', function(request, response){
+  console.log('USING POST METHOD');
   handleRequest(request, response);
 });
 
+//for clicking on it
+app.get('/checkUser', function(request, response){
+  console.log('USING GET METHOD');
+  handleRequest(request, response);
+});
+
+app.get('/create', function(request, response){
+  console.log('USING GET METHOD for create');
+  response.render('pages/create-goal');
+});
+
 function handleRequest(request, response) {
-console.log(request.body.username);
-  const username = request.body.username;
-  pool.query('SELECT * FROM "users" WHERE user_name = $1', [username], (err, res) => {
+
+if (request.body.username != null && request.body.password != null) {
+  request.session.user = request.body.username;
+  request.session.pass = request.body.password;
+  console.log(request.body.username, request.body.password);
+  console.log(request.session.user, request.session.pass);
+} 
+if (request.session.user != "" && request.session.pass != "") {
+const pass = request.session.pass;
+const username = request.session.user;
+  pool.query('SELECT * FROM "users" WHERE user_name = $1 and password = $2', [username, pass], (err, res) => {
     if (err) {
-      throw err
+      console.log("User does not exist");
+      request.session.destroy();
+      throw err;
     } else {
-      console.log(res);
-      //response.json(res.rows[0]);
+      if (res.rows[0].user_name != username || res.rows[0].password != pass) {
+      request.session.destroy();
+      response.render('pages/index');
+    } 
       var user = res.rows[0];
+      request.session.uid = res.rows[0].id;
+      console.log("Is this the right id? " + request.session.uid);
       console.log(user);
       showGoals(response, user);
     }
   })
+} else {
+  response.render('pages/index');
+}
 }
 
 function showGoals(response, user) {
@@ -73,35 +108,15 @@ function showGoals(response, user) {
     if (err) {
       throw err
     } else {
-      console.log(res);
       var goals = [];
-      //response.json(res.rows[0]);
       for (i = 0; i < res.rows.length; i++) {
-        console.log(res.rows[i]);
         goals.push(res.rows[i]);
-        console.log(goals[i]);
       } 
-      //var goals[0] = res.rows[0];
       var params = {user: user, goals: goals};
       response.render('pages/goals', params);
     }
   })
-  //var params = {user: user, goals: goals};
-  //response.render('pages/goals', params);
 }
-
-app.get('/goals', function(request, response){
-  const id = request.query.id;
-  pool.query('SELECT * FROM "goals" WHERE id = $1', [id], (err, res) => {
-    if (err) {
-      throw err
-    } else {
-      console.log(res);
-      response.json(res.rows[0]);
-    }
-  })
-  
-});
 
 app.get('/map', function(request, response){
   var place = 'rexburg walmart';//'1600 Amphitheatre Parkway, Mountain View, CA';
@@ -123,3 +138,84 @@ app.get('/map', function(request, response){
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+//Insert Goals
+
+app.post('/insertGoal', function(request, response){
+  console.log('USING POST METHOD To Insert Goal');
+  handleInsert(request, response);
+});
+
+function handleInsert(request, response) {
+
+if (request.body.username != null && request.body.password != null) {
+  request.session.user = request.body.username;
+  request.session.pass = request.body.password;
+  console.log(request.body.username, request.body.password);
+  console.log(request.session.user, request.session.pass);
+}
+if (request.session.user != "" && request.session.pass != "") {
+  const pass = request.session.pass;
+  const username = request.session.user;
+  const user_id = request.session.uid;
+  const goalName = request.body.goal_name;
+  const description = request.body.description;
+  const category = request.body.goal_view;
+  const endDate = request.body.end_date;
+  const location = request.body.location;
+  console.log(user_id, goalName, description, endDate, category, location);
+
+    pool.query('INSERT INTO goals(user_id, goal_name, description, start_date, end_date, goal_count, total_count, category, location) VALUES($1, $2, $3, current_timestamp, $4, 0, 0, $5, $6)', 
+    [user_id, goalName, description, endDate, category, location], (err, res) => {
+      if (err) {
+        console.log("did not insert goal");
+        request.session.destroy();
+        throw err;
+      } else {
+        response.render('pages/confirmation');
+      }
+    })
+  }
+}
+
+//Logout
+app.get('/logout', handleLogout);
+
+function handleLogout(request, response) {
+
+  // We should do better error checking here to make sure the parameters are present
+  if (request.session.user) {
+    console.log("destroying session");
+    request.session.destroy();
+    response.render('pages/index');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
